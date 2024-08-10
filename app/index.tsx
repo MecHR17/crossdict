5fa3622fdb174d7e273868369ef6f22eb5417907
MecHR17 <asrin17cankaya@hotmail.com>
2024-08-10 04:05:33 +0300

    Firebase config

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,20 @@ import { Text, View } from "react-native";
 import { Link } from "expo-router";
 import { ScrollView } from "react-native-gesture-handler";
 
+import { getDatabase, ref, onValue, get, child,set } from "firebase/database";
+import {app} from "@/constants/firebase"
+
 export default function Index() {
+  const dbRef = ref(getDatabase());
+  get(child(dbRef, 'words/')).then((snapshot) => {
+    if (snapshot.exists()) {
+      console.log(snapshot.val());
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
   return (
     <View
       style={{
---
+++ constants/firebase.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA1wTbnD13Idj8Q_h5C8IMiV_nMhycYPj8",
+  authDomain: "crossdict-80c93.firebaseapp.com",
+  projectId: "crossdict-80c93",
+  storageBucket: "crossdict-80c93.appspot.com",
+  messagingSenderId: "718887298459",
+  appId: "1:718887298459:web:b2533040ec239fd4f7ed3e",
+  databaseURL: "https://crossdict-80c93-default-rtdb.europe-west1.firebasedatabase.app"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
